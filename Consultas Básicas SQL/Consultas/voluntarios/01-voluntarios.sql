77365703b1481b1a76cb40361c8d47f0056b0ddb
Kerjox <kerjoxgamer095@gmail.com>
2021-01-12 01:58:15 +0100

    12-ene-21

---
+++ Consultas Básicas SQL/Consultas/voluntarios/01-voluntarios.sql
@@ -0,0 +1,3 @@
+SELECT vol.*
+FROM voluntarios AS vol, localidades AS loc, provincias AS prov, paises AS pais
+WHERE vol.idLocalidad = loc.idLocalidad AND loc.idProvincia = prov.idProvincia AND prov.idPais = pais.idPais AND pais.pais = 'Extranjeros'
